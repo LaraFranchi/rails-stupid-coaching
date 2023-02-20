108f2abe6c9f43ff1817e239a66e69e2f7ef0c70
Lara Franchi <laradeandrade@protonmail.com>
2023-02-20 12:23:51 -0500

    StupidCoach

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # verb "url", to: "controller#action"
+  get '/ask', to: 'controller#ask'
 
   # Defines the root path route ("/")
   # root "articles#index"
